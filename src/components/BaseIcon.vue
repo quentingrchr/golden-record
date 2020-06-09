434cc0f6b2bbf707e4acd3f6c6f73fb797bc3666
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-06-09 11:01:45 +0200

    add svg

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg v-on="$listeners">
+        <use :href="href.startsWith('#') ? href : `#${href}`" />
+    </svg>
+</template>
+<script>
+export default {
+    name: "BaseIcon",
+    props: {
+        href: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
