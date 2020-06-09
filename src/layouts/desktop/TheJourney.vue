91654b6cc19ec20d2bc2d3301a94eb66abba6172
bastien17-dev <bastien.paquier@gmail.com>
2020-06-10 01:04:01 +0200

    first version of visual content frame

--- src/layouts/desktop/TheJourney.vue
+++ src/layouts/desktop/TheJourney.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1>The journey</h1>
-    <img
-      src="https://raw.githubusercontent.com/bastien17-dev/docWeb-voyager-back-end/master/assets/voyagerImg/images-golden-record-voyager-01.jpg"
-    />
-  </div>
+  <h1>The journey</h1>
 </template>
 
 <script>
