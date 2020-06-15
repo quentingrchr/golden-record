4cd83bc3e8c580c6623089ed3e24ea4d575a9a89
bastien17-dev <bastien.paquier@gmail.com>
2020-06-15 20:25:23 +0200

    game v2

--- src/layouts/desktop/Game.vue
+++ src/layouts/desktop/Game.vue
@@ -2,8 +2,58 @@
   <div class="game">
     <div class="stars"></div>
     <div class="twinkling"></div>
-    <div class="game__playground" ref="playground">
-      <div class="star" ref="star"></div>
+    <div class="game__playground playground" ref="playground" v-if="!failed">
+      <div class="playground__item item">
+        <p class="item__description">
+          <strong>Neptune</strong> <br /><strong>25.08.1989</strong
+          ><br />Distance: 4.950km<br />from the surface<br />Voyager 2
+        </p>
+        <img
+          src="./../../assets/img/planets/neptune.png"
+          class="item__obstacle neptune"
+        />
+      </div>
+      <div class="playground__item item">
+        <img
+          src="./../../assets/img/planets/uranus.png"
+          class="item__obstacle uranus"
+        />
+        <p class="item__description">
+          <strong>Uranus</strong> <br /><strong>24.01.1986</strong
+          ><br />Distance: 81.500km<br />from the surface<br />Voyager 2
+        </p>
+      </div>
+      <div class="playground__item item">
+        <p class="item__description">
+          <strong>Saturne</strong> <br /><strong>10.11.1980</strong
+          ><br />Distance: 124.000km<br />from the surface<br />Voyager 1
+        </p>
+        <img
+          src="./../../assets/img/planets/saturne.png"
+          class="item__obstacle saturne"
+        />
+      </div>
+      <div class="playground__item item">
+        <img
+          src="./../../assets/img/planets/jupiter.png"
+          class="item__obstacle jupiter"
+        />
+        <p class="item__description">
+          <strong>Jupiter</strong> <br /><strong>5.03.1979</strong
+          ><br />Distance: 278.000km<br />from the surface<br />Voyager 1
+        </p>
+      </div>
+      <div class="playground__item item info">
+        <h2 class="item__description">
+          Don't dump into the planets 🌎
+        </h2>
+      </div>
+      <div class="playground__item item info">
+        <h1 class="item__description">
+          1977 Let's Start
+        </h1>
+      </div>
+
       <img
         src="./../../assets/img/voyager_journey.png"
         ref="voyager"
@@ -11,9 +61,15 @@
       />
     </div>
     <transition name="fade">
-      <div class="looseOverlay" v-if="failed">
+      <div class="overlay" v-if="failed">
         <h3>Voyager is destroy, your a shit</h3>
         <button @click="play" class="play">Try again</button>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="overlay" v-if="intro">
+        <h3>Press key "Q" to go left <br />Press key "D" to go right</h3>
+        <button @click="play" class="play">Play</button>
       </div>
     </transition>
   </div>
@@ -26,9 +82,10 @@ export default {
       rightLimit: window.innerWidth - 100,
       leftLimit: 65,
       voyagerMoveSpeed: 10,
-      gameSpeed: 200,
+      gameSpeed: 8,
       template: null,
       failed: false,
+      intro: true,
     };
   },
   methods: {
@@ -52,30 +109,26 @@ export default {
     },
     starFalling() {
       this.$refs.playground.style.top =
-        (this.$refs.playground.offsetTop + 30).toString() + 'px';
+        (this.$refs.playground.offsetTop + 1).toString() + 'px';
     },
-    collision($el) {
-      console;
-      const elementTop = $el.offsetTop + $el.parentNode.offsetTop;
-
-      if (
-        elementTop <
-          this.$refs.voyager.offsetTop + this.$refs.voyager.offsetHeight &&
-        elementTop + $el.offsetHeight >= this.$refs.voyager.offsetTop &&
-        $el.offsetLeft + $el.offsetWidth > this.$refs.voyager.offsetLeft &&
-        $el.offsetLeft <
-          this.$refs.voyager.offsetLeft + this.$refs.voyager.offsetWidth
-      ) {
-        this.loose();
-      }
+    collision() {
+      document.querySelectorAll('.item__obstacle').forEach(($el) => {
+        const elementTop = $el.offsetTop + this.$refs.playground.offsetTop;
+        if (
+          elementTop <
+            this.$refs.voyager.offsetTop + this.$refs.voyager.offsetHeight &&
+          elementTop + $el.offsetHeight >= this.$refs.voyager.offsetTop &&
+          $el.offsetLeft + $el.offsetWidth > this.$refs.voyager.offsetLeft &&
+          $el.offsetLeft <
+            this.$refs.voyager.offsetLeft + this.$refs.voyager.offsetWidth
+        ) {
+          this.loose();
+        }
+      });
     },
     loose() {
+      console.log('lose');
       this.failed = true;
-      this.$refs.playground.style.top =
-        (
-          (this.$refs.playground.offsetHeight - window.innerHeight) *
-          -1
-        ).toString() + 'px';
       clearInterval(this.template);
       this.template = null;
     },
@@ -86,6 +139,7 @@ export default {
       }, this.gameSpeed);
       this.template = gameTemplate;
       this.failed = false;
+      this.intro = false;
     },
   },
   created() {
@@ -94,13 +148,7 @@ export default {
   destroyed() {
     window.removeEventListener('keydown', this.moveVoyager);
   },
-  mounted() {
-    const gameTemplate = setInterval(() => {
-      this.starFalling();
-      this.collision(this.$refs.star);
-    }, this.gameSpeed);
-    this.template = gameTemplate;
-  },
+
   beforeDestroy() {
     clearInterval(this.template);
   },
@@ -109,6 +157,7 @@ export default {
 
 <style lang="scss" scoped>
 .game {
+  position: relative;
   width: 100%;
   height: 100%;
   background-color: white;
@@ -117,23 +166,70 @@ export default {
 
 @keyframes moveVoyager {
   to {
-    transform: translateY(50%);
+    transform: translateY(20%);
   }
-}
-
-.game__playground {
-  position: relative;
-  height: 110vh;
-  width: 100%;
-  bottom: 0;
 }
 
 .voyager {
   position: fixed;
-  left: 50%;
-  bottom: 10vh;
-  width: 100px;
+  left: 80%;
+
+  bottom: 20vh;
+  width: 200px;
   animation: moveVoyager 1s ease-in-out alternate infinite forwards;
+}
+
+.game__playground {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 50px;
+  margin-bottom: 50vh;
+
+  & .playground__item {
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 400px;
+
+    &.info {
+      display: inline;
+    }
+
+    & .item__obstacle {
+      border-radius: 50%;
+
+      &.jupiter {
+        width: 70%;
+      }
+
+      &.saturne {
+        width: 50%;
+      }
+
+      &.uranus {
+        width: 50%;
+      }
+
+      &.neptune {
+        width: 40%;
+      }
+    }
+
+    & .item__description {
+      color: grey;
+      opacity: 0.4;
+
+      & strong {
+        font-size: 1.2rem;
+        font-weight: bold;
+      }
+    }
+  }
 }
 
 .star {
@@ -145,7 +241,7 @@ export default {
   left: 50%;
 }
 
-.looseOverlay {
+.overlay {
   position: absolute;
   width: 100vw;
   height: 100vh;
@@ -156,6 +252,8 @@ export default {
   align-items: center;
   justify-content: center;
   background-color: rgba(0, 0, 0, 0.8);
+  margin-left: 50px;
+  line-height: 3rem;
 }
 
 .play {
