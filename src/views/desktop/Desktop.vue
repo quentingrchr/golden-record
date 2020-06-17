d620bce4861b7ce76f66dda1e950d45bd666fbee
bastien17-dev <bastien.paquier@gmail.com>
2020-06-17 15:13:12 +0200

    remove scroll behaviour

--- src/views/desktop/Desktop.vue
+++ src/views/desktop/Desktop.vue
@@ -1,6 +1,5 @@
 <template>
-  <main @wheel="wheel" class="desktopViews-container">
-
+  <main class="desktopViews-container">
     <Explaination
       v-on:closeinstruction="closeInstruction"
       v-show="currentPage === 1"
@@ -22,25 +21,25 @@
       <Team v-else-if="currentPage === 5" key="team" />
       <Game v-else-if="currentPage === 6" key="game" />
     </transition>
-    <NavBar :page="currentPage" :scroll="scrollSpeed" @jumpToOtherChapter="changeChapter" />
+    <NavBar :page="currentPage" @jumpToOtherChapter="changeChapter" />
     <Icon />
     <ModalsManager />
   </main>
 </template>
 
 <script>
-import EventBus from "@/EventBus";
-import NavBar from "@/components/NavBar__Desktop.vue";
-import DiscoverImg from "@/layouts/desktop/DiscoverImg.vue";
-import DiscoverSound from "@/layouts/desktop/DiscoverSound.vue";
-import Explaination from "@/layouts/desktop/Explaination.vue";
+import EventBus from '@/EventBus';
+import NavBar from '@/components/NavBar__Desktop.vue';
+import DiscoverImg from '@/layouts/desktop/DiscoverImg.vue';
+import DiscoverSound from '@/layouts/desktop/DiscoverSound.vue';
+import Explaination from '@/layouts/desktop/Explaination.vue';
 
-import HowToUseIt from "@/layouts/desktop/HowToUseIt.vue";
-import Team from "@/layouts/desktop/Team.vue";
-import TheJourney from "@/layouts/desktop/TheJourney.vue";
-import Game from "@/layouts/desktop/Game.vue";
-import Icon from "@/components/Icons.vue";
-import ModalsManager from "@/components/modals/ModalsManager.vue";
+import HowToUseIt from '@/layouts/desktop/HowToUseIt.vue';
+import Team from '@/layouts/desktop/Team.vue';
+import TheJourney from '@/layouts/desktop/TheJourney.vue';
+import Game from '@/layouts/desktop/Game.vue';
+import Icon from '@/components/Icons.vue';
+import ModalsManager from '@/components/modals/ModalsManager.vue';
 
 export default {
   components: {
@@ -53,135 +52,41 @@ export default {
     Icon,
     ModalsManager,
     Game,
-    Explaination
+    Explaination,
   },
   data() {
     return {
       instructionIsClosed: false,
-      wheelCount: 0,
-      // You just have to change he value below to set the scroll' sensitive rate (the highest is less sensitive)
-      scrollSpeed: 6,
       goingNextPart: true,
       pageMoveNext: true,
-      speaking: false
+      speaking: false,
+      currentPage: 1,
     };
-  },
-  computed: {
-    currentPage: function() {
-      if (this.wheelCount < this.scrollSpeed * 10) {
-        return 1;
-      } else if (
-        this.wheelCount >= this.scrollSpeed * 10 &&
-        this.wheelCount < this.scrollSpeed * 20
-      ) {
-        return 2;
-      } else if (
-        this.wheelCount >= this.scrollSpeed * 20 &&
-        this.wheelCount < this.scrollSpeed * 30
-      ) {
-        return 3;
-      } else if (
-        this.wheelCount >= this.scrollSpeed * 30 &&
-        this.wheelCount < this.scrollSpeed * 40
-      ) {
-        return 4;
-      } else if (
-        this.wheelCount >= this.scrollSpeed * 40 &&
-        this.wheelCount < this.scrollSpeed * 50
-      ) {
-        return 5;
-      } else {
-        return 6;
-      }
-    }
   },
   methods: {
     closeInstruction() {
       this.instructionIsClosed = true;
     },
-    wheel(e) {
-      e.preventDefault();
-      EventBus.$emit("close");
-      if (this.wheelCount >= 0 && this.wheelCount <= this.scrollSpeed * 50) {
-        if (e.deltaY < 0 || e.deltaX < 0) {
-          this.wheelCount--;
-          this.pageMoveNext = false;
-          return;
-        } else {
-          this.wheelCount++;
-          this.pageMoveNext = true;
-          return;
-        }
-      } else if (this.wheelCount < 0) {
-        this.wheelCount = 0;
-      } else {
-        this.wheelCount = this.scrollSpeed * 60;
-      }
-    },
     changeChapter(value) {
-      value > this.wheelCount
+      value > this.currentPage
         ? (this.pageMoveNext = true)
         : (this.pageMoveNext = false);
-      this.wheelCount = value;
+      this.currentPage = value;
     },
     changeChapterWithKeyboard(e) {
-      if (e.key === "ArrowRight") {
-        if (this.wheelCount < this.scrollSpeed * 10) {
-          this.changeChapter(this.scrollSpeed * 15);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 10 &&
-          this.wheelCount < this.scrollSpeed * 20
-        ) {
-          this.changeChapter(this.scrollSpeed * 25);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 20 &&
-          this.wheelCount < this.scrollSpeed * 30
-        ) {
-          this.changeChapter(this.scrollSpeed * 35);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 30 &&
-          this.wheelCount < this.scrollSpeed * 40
-        ) {
-          this.changeChapter(this.scrollSpeed * 45);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 40 &&
-          this.wheelCount < this.scrollSpeed * 50
-        ) {
-          this.changeChapter(this.scrollSpeed * 55);
-        }
-      } else if (e.key === "ArrowLeft") {
-        if (
-          this.wheelCount >= this.scrollSpeed * 50 &&
-          this.wheelCount < this.scrollSpeed * 60
-        ) {
-          this.changeChapter(this.scrollSpeed * 45);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 40 &&
-          this.wheelCount < this.scrollSpeed * 50
-        ) {
-          this.changeChapter(this.scrollSpeed * 35);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 30 &&
-          this.wheelCount < this.scrollSpeed * 40
-        ) {
-          this.changeChapter(this.scrollSpeed * 25);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 20 &&
-          this.wheelCount < this.scrollSpeed * 30
-        ) {
-          this.changeChapter(this.scrollSpeed * 15);
-        } else if (
-          this.wheelCount >= this.scrollSpeed * 10 &&
-          this.wheelCount < this.scrollSpeed * 20
-        ) {
-          this.changeChapter(this.scrollSpeed * 5);
-        }
+      if (e.key === 'ArrowRight') {
+        this.currentPage >= 6 ? (this.currentPage = 6) : this.currentPage++;
+      } else if (e.key === 'ArrowLeft') {
+        this.currentPage <= 1 ? (this.currentPage = 1) : this.currentPage--;
       }
-    }
+    },
+  },
+  created() {
+    window.addEventListener('keydown', this.changeChapterWithKeyboard);
   },
   destroyed() {
-    window.removeEventListener("keydown", this.changeChapterWithKeyboard);
-  }
+    window.removeEventListener('keydown', this.changeChapterWithKeyboard);
+  },
 };
 </script>
 
